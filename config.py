b595baf418c64adeb6a87b372d1079154eebaaca
Amal <87772836+amaldeeps7@users.noreply.github.com>
2024-03-31 01:58:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:digiplex@localhost/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
